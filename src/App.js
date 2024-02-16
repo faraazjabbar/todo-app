898f5055d2a30694445e7b28c8435310f47c8e06
fajabbar <fahmedjabbar@deloitte.com>
2024-02-16 13:40:05 +0530

    app changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+// Conditionals
+// Lists
+// Api call
+// Context api
+
+// const isLoggedIn = true;
+// const isAdmin = false;
 
 function App() {
+  let [todos, setTodos] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((json) => setTodos(json));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          {todo.title}{" "}
+          {todo.isCompleted ? (
+            <></>
+          ) : (
+            <svg
+              fill="#000000"
+              width="20px"
+              height="20px"
+              viewBox="0 0 1920 1920"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M1827.701 303.065 698.835 1431.801 92.299 825.266 0 917.564 698.835 1616.4 1919.869 395.234z"
+                fill-rule="evenodd"
+              />
+            </svg>
+          )}
+        </li>
+      ))}
+    </ul>
   );
 }
 
